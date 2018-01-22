4af49fffdfc8693350a8692da46ccad5f420431e
Ken Ijiwoye <me@kwoye.me>
2018-01-21 20:24:26 -0700

    upgrade to rails 5.1.4, fixed breaking changes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,8 +9,8 @@ class ApplicationController < ActionController::Base
   #strong params
   protected
   	def configure_permitted_parameters
-  		devise_parameter_sanitizer.for(:sign_up) {|u| u.permit(:username, :email, :password, :password_confirmation, :remember_me)}
-  		devise_parameter_sanitizer.for(:sign_in) {|u| u.permit(:username, :email, :password, :password_confirmation, :remember_me)}
-  		devise_parameter_sanitizer.for(:account_update) {|u| u.permit(:email, :password, :password_confirmation, :remember_me)}
+  		devise_parameter_sanitizer.permit(:sign_up) {|u| u.permit(:username, :email, :password, :password_confirmation, :remember_me)}
+  		devise_parameter_sanitizer.permit(:sign_in) {|u| u.permit(:username, :email, :password, :password_confirmation, :remember_me)}
+  		devise_parameter_sanitizer.permit(:account_update) {|u| u.permit(:email, :password, :password_confirmation, :remember_me)}
   	end
 end
